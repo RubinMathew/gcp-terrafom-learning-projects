69c7c96000a65b85d2d0b76cf6f0a9857ce8c9fc
RubinMathew <78633996+RubinMathew@users.noreply.github.com>
2023-10-09 17:06:02 +0530

    Update provider.tf

--- 05_webserver_mig_loadbalancer/provider.tf
+++ 05_webserver_mig_loadbalancer/provider.tf
@@ -1,7 +1,7 @@
 # Configure the Google Cloud provider- for projetc my-first-project using default-compute-service-account
 
 provider "google" {
-  project     = "<your project name" #specify your project name
+  project     = "<your project name>" #specify your project name
   region      = "us-central1"
   zone ="us-central1-a"
   # credentials = file("<path-to-key-file>.json") or provide your key as environment variable with name GOOGLE_APPLICATION_CREDENTIALS
