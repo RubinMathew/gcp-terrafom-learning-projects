d23a5c5d197b53e3aa60fd075e4b57021a3bfcae
RUBIN MATHEW <rubinullattu129@gmail.com>
2023-10-09 10:27:43 +0530

    Webserver using input-output varibales has been committed

---
+++ 02_web_serrver_using_input_ouput_variables/outputs.tf
@@ -0,0 +1,14 @@
+# Output the instance status,name,internat & external IP address of the created VM
+output "Instance_Name" {
+  value = google_compute_instance.instance-tf-rbn-pc.name
+}
+
+output "Instance_Status" {
+  value = google_compute_instance.instance-tf-rbn-pc.current_status
+}
+output "Internal_ip" {
+  value = google_compute_instance.instance-tf-rbn-pc.network_interface.0.network_ip
+}
+output "External_ip" {
+  value = google_compute_instance.instance-tf-rbn-pc.network_interface[0].access_config[0].nat_ip
+}
